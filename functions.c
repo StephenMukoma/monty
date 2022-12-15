8d1546e7324317ec490a2728eba90354c65bdfb5
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-15 17:20:19 +0000

    feat, implement push and pall

--- functions.c
+++ functions.c
@@ -1,1 +1,49 @@
 #include "monty.h"
+/**
+*push - pushes an element to the stack
+*
+*@stack: pointer to list
+*@line_number: line to be executed
+*/
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	(void)line_number;
+	new = malloc(sizeof(stack_t));
+
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		clear(stack);
+		exit(EXIT_FAILURE);
+	}
+
+		new->n = global.element;
+		new->prev = NULL;
+		new->next = *stack;
+		*stack = new;
+
+	if (new->next != NULL)
+		new->next->prev = new;
+}
+/**
+*pall - prints all values on the stack
+*
+*@line_number: line being executed
+*@stack:pointer to list
+*/
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)line_number;
+	if (*stack == NULL)
+		return;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
