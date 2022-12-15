9477aecb41d10f6e4841ac07c828335b0d49e384
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-15 19:18:12 +0000

    feat, implement the div opcode

--- 2_functions.c
+++ 2_functions.c
@@ -69,3 +69,44 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 }
+/**
+*_div - divides top two element
+*
+*@stack: pointer to list
+*@line_number: line no being executed
+*/
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int counter = 0, temp;
+	stack_t *curr = *stack;
+
+	if (curr)
+	{
+		while (curr)
+		{
+			curr = curr->next;
+			counter++;
+		}
+		if (counter < 2)
+		{
+			fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+			clear(stack);
+			exit(EXIT_FAILURE);
+		}
+		if ((*stack)->n == 0)
+		{
+			fprintf(stderr, "L%u: division by zero\n", line_number);
+			clear(stack);
+			exit(EXIT_FAILURE);
+		}
+		temp = (*stack)->n;
+		pop(stack, line_number);
+		(*stack)->n = (*stack)->n / temp;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		clear(stack);
+		exit(EXIT_FAILURE);
+	}
+}
--- monty.h
+++ monty.h
@@ -51,6 +51,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
 
 int str_cmp(char *str1, char *str2);
 int len(char **c);
